fa7a55775405061aa1f6f9f4b0996356249f3a7a
zakeer <shaikzakeerahmad@gmail.com>
2026-03-12 09:07:54 +0530

    zakeer Added UC5 Stack Palindrome

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PalindroneCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Use Case 5 : Stack Based Palindrome Checker
+
+Objective:
+To check whether a string is palindrome using Stack Data Structure.
+
+Flow:
+1. Read input string
+2. Push characters into stack
+3. Pop characters to reverse string
+4. Compare original and reversed string
+5. Print resultgit status
